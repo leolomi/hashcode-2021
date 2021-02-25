3d1d4c11c4648260cdac4a80f088cccc376bd8e3
LeHic <loic.madies@gmail.com>
2021-02-25 20:16:57 +0100

    init obj

--- src/main/java/hashcode/business/PizzaBusiness.java
+++ src/main/java/hashcode/business/PizzaBusiness.java
@@ -1,13 +1,26 @@
 package hashcode.business;
 
+import java.util.Map;
+
 import hashcode.model.InputFile;
+import hashcode.model.Intersection;
 import hashcode.model.OutputFile;
+import hashcode.model.Street;
 
 public class PizzaBusiness {
 
 	public static OutputFile deliverPizzas(InputFile inputFile) {
 
-		return new OutputFile();
+		final OutputFile out = new OutputFile();
+
+		for (final Map.Entry<Integer, Intersection> entry : inputFile.getIntersectionMap().entrySet()) {
+			for(final Street street : entry.getValue().getIncommingStreets()) {
+				street.setTimeOnTrafficLight(1);
+			}
+			out.getIntersections().add(entry.getValue());
+		}
+
+		return out;
 	}
 
 }
--- src/test/java/hashcode/PizzaAppTest.java
+++ src/test/java/hashcode/PizzaAppTest.java
@@ -20,20 +20,48 @@ public class PizzaAppTest {
 
 	private static final String FILE_A_IN = "a.txt";
 
-	private static final String FILE_A_OUT = "a_example.out";
+	private static final String FILE_B_IN = "b.txt";
+
+	private static final String FILE_C_IN = "c.txt";
+
+	private static final String FILE_D_IN = "d.txt";
+
+	private static final String FILE_E_IN = "e.txt";
+
+	private static final String FILE_A_OUT = "a.out";
+
+	private static final String FILE_B_OUT = "b.out";
+
+	private static final String FILE_C_OUT = "c.out";
+
+	private static final String FILE_D_OUT = "d.out";
+
+	private static final String FILE_E_OUT = "e.out";
 
 	@Test
 	public void pizzaAppTest() throws NumberFormatException, IOException {
 
 		// Read input file
 		final InputFile inputFileA = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_A_IN);
+		final InputFile inputFileB = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_B_IN);
+		final InputFile inputFileC = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_C_IN);
+		final InputFile inputFileD = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_D_IN);
+		final InputFile inputFileE = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_E_IN);
 
 		// Business
 		final OutputFile outputFileA = PizzaBusiness.deliverPizzas(inputFileA);
+		final OutputFile outputFileB = PizzaBusiness.deliverPizzas(inputFileB);
+		final OutputFile outputFileC = PizzaBusiness.deliverPizzas(inputFileC);
+		final OutputFile outputFileD = PizzaBusiness.deliverPizzas(inputFileD);
+		final OutputFile outputFileE = PizzaBusiness.deliverPizzas(inputFileE);
 
 
 		// Write output file
 		TrafficLightUtils.writeOutputFile(outputFileA, PATH_OUTPUT_FILES + FILE_A_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileB, PATH_OUTPUT_FILES + FILE_B_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileC, PATH_OUTPUT_FILES + FILE_C_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileD, PATH_OUTPUT_FILES + FILE_D_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileE, PATH_OUTPUT_FILES + FILE_E_OUT);
 
 	}
 }
