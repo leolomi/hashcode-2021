e89c7f69f1fbdd4f92aa732c83363b930caef84a
LeHic <loic.madies@gmail.com>
2021-02-25 20:21:52 +0100

    init obj

--- src/test/java/hashcode/TrafficLightAppTest.java
+++ src/test/java/hashcode/TrafficLightAppTest.java
@@ -1,7 +1,5 @@
 package hashcode;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
 
 import org.junit.Test;
@@ -15,57 +13,64 @@ import hashcode.utils.TrafficLightUtils;
  * Unit test for simple App.
  */
 public class TrafficLightAppTest {
-	
-	private static final String PATH_INPUT_FILES = "src/test/resources/input/";
-	
-	private static final String PATH_OUTPUT_FILES = "src/test/resources/output/";
-	
-	private static final String FILE_A_IN = "a.txt";
-	
-	private static final String FILE_B_IN = "b.txt";
-	
-	private static final String FILE_C_IN = "c.txt";
-	
-	private static final String FILE_D_IN = "d.txt";
-	
-	private static final String FILE_E_IN = "e.txt";
-	
-	private static final String FILE_A_OUT = "a.out";
-	
-	private static final String FILE_B_OUT = "b.out";
-	
-	private static final String FILE_C_OUT = "c.out";
-	
-	private static final String FILE_D_OUT = "d.out";
-	
-	private static final String FILE_E_OUT = "e.out";
-    
-    @Test
-    public void pizzaAppTest() throws NumberFormatException, IOException {
 
-    	// Read input file
-        InputFile inputFileA = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_A_IN);
-        InputFile inputFileB = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_B_IN);
-        InputFile inputFileC = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_C_IN);
-        InputFile inputFileD = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_D_IN);
-        InputFile inputFileE = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_E_IN);
-        
-        // Business
-        OutputFile outputFileA = TrafficLightBusiness.setTrafficLightTime(inputFileA);
-        OutputFile outputFileB = TrafficLightBusiness.setTrafficLightTime(inputFileB);
-        OutputFile outputFileC = TrafficLightBusiness.setTrafficLightTime(inputFileC);
-        OutputFile outputFileD = TrafficLightBusiness.setTrafficLightTime(inputFileD);
-        OutputFile outputFileE = TrafficLightBusiness.setTrafficLightTime(inputFileE);
-        
-        // Write output file
-        TrafficLightUtils.writeOutputFile(outputFileA, PATH_OUTPUT_FILES + FILE_A_OUT);
-        TrafficLightUtils.writeOutputFile(outputFileB, PATH_OUTPUT_FILES + FILE_B_OUT);
-        TrafficLightUtils.writeOutputFile(outputFileC, PATH_OUTPUT_FILES + FILE_C_OUT);
-        TrafficLightUtils.writeOutputFile(outputFileD, PATH_OUTPUT_FILES + FILE_D_OUT);
-        TrafficLightUtils.writeOutputFile(outputFileE, PATH_OUTPUT_FILES + FILE_E_OUT);
-        
-        //Integer totalScore = outputFileA.getScore() + outputFileB.getScore() + outputFileC.getScore() + outputFileD.getScore() + outputFileE.getScore();  
-        
-        //System.out.println("Total score all files " + totalScore);
-    }
+	private static final String PATH_INPUT_FILES = "src/test/resources/input/";
+
+	private static final String PATH_OUTPUT_FILES = "src/test/resources/output/";
+
+	private static final String FILE_A_IN = "a.txt";
+
+	private static final String FILE_B_IN = "b.txt";
+
+	private static final String FILE_C_IN = "c.txt";
+
+	private static final String FILE_D_IN = "d.txt";
+
+	private static final String FILE_E_IN = "e.txt";
+
+	private static final String FILE_F_IN = "f.txt";
+
+	private static final String FILE_A_OUT = "a.out";
+
+	private static final String FILE_B_OUT = "b.out";
+
+	private static final String FILE_C_OUT = "c.out";
+
+	private static final String FILE_D_OUT = "d.out";
+
+	private static final String FILE_E_OUT = "e.out";
+
+	private static final String FILE_F_OUT = "f.out";
+
+	@Test
+	public void pizzaAppTest() throws NumberFormatException, IOException {
+
+		// Read input file
+		final InputFile inputFileA = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_A_IN);
+		final InputFile inputFileB = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_B_IN);
+		final InputFile inputFileC = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_C_IN);
+		final InputFile inputFileD = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_D_IN);
+		final InputFile inputFileE = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_E_IN);
+		final InputFile inputFileF = TrafficLightUtils.readInputFile(PATH_INPUT_FILES + FILE_F_IN);
+
+		// Business
+		final OutputFile outputFileA = TrafficLightBusiness.setTrafficLightTime(inputFileA);
+		final OutputFile outputFileB = TrafficLightBusiness.setTrafficLightTime(inputFileB);
+		final OutputFile outputFileC = TrafficLightBusiness.setTrafficLightTime(inputFileC);
+		final OutputFile outputFileD = TrafficLightBusiness.setTrafficLightTime(inputFileD);
+		final OutputFile outputFileE = TrafficLightBusiness.setTrafficLightTime(inputFileE);
+		final OutputFile outputFileF = TrafficLightBusiness.setTrafficLightTime(inputFileF);
+
+		// Write output file
+		TrafficLightUtils.writeOutputFile(outputFileA, PATH_OUTPUT_FILES + FILE_A_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileB, PATH_OUTPUT_FILES + FILE_B_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileC, PATH_OUTPUT_FILES + FILE_C_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileD, PATH_OUTPUT_FILES + FILE_D_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileE, PATH_OUTPUT_FILES + FILE_E_OUT);
+		TrafficLightUtils.writeOutputFile(outputFileF, PATH_OUTPUT_FILES + FILE_F_OUT);
+
+		//Integer totalScore = outputFileA.getScore() + outputFileB.getScore() + outputFileC.getScore() + outputFileD.getScore() + outputFileE.getScore();
+
+		//System.out.println("Total score all files " + totalScore);
+	}
 }
